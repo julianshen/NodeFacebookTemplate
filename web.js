9d1dbffda3c7508400c5f8bbbccc6f2059f30511
julianshen <julianshen@gmail.com>
2012-04-23 00:44:08 +0800

    Facebook

--- web.js
+++ web.js
@@ -5,19 +5,46 @@ var app = express.createServer(
   express.logger(),
   express.static(__dirname + '/public'),
   express.bodyParser(),
-  express.cookieParser()
+  express.cookieParser(),
   // set this to a secret value to encrypt session cookies
-  // express.session({ secret: process.env.SESSION_SECRET || 'secret123' })
+  express.session({ secret: process.env.SESSION_SECRET || 'mysecret11' }),
+  require('faceplate').middleware({
+    app_id: process.env.FACEBOOK_APPID,
+    secret: process.env.FACEBOOK_SECRET
+  })
 );
 
+
 app.set('view engine', 'ejs');
+
+
+//Routing
+//Index
 app.get('/', function(req,res) {
   res.render('index.ejs', {
-        layout:    false,
+        layout:    'layouts/facebook_common.ejs',
         req:       req,
         app:       app,
       });
 });
 
+//Facebook channel files
+app.get('/_channel', function(req, res) {
+  var cache_expire = 60*60*24*365;
+  res.header('Pragma: public');
+  res.header('Cache-Control: max-age='+cache_expire);
+  res.header('Content-type: text/html');
+  res.send('<script src="//connect.facebook.net/en_US/all.js"></script>');
+});
 
-app.listen(3000);
+//For test
+app.get('/__events', function(req, res) {
+  req.facebook.get('/me/events', { limit: 4 }, function(events) {
+    res.send('events: ' + require('util').inspect(events));
+  });
+});
+
+
+// listen to the PORT given to us in the environment
+var port = process.env.PORT || 3000;
+app.listen(port);
